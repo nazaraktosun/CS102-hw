4187fe39fdd47492a3e749d4c95859c4f911ca2d
aysegul.yldz <aysegul.yildiz@ug.bilkent.edu.tr>
2022-09-21 14:52:03 +0300

    max-min class

--- test.java
+++ test.java
@@ -6,6 +6,7 @@ int[] Array = new int[]{ 1,2,3,4,5,6,7,8,9,10 };
 public class test {
     
     public static void main(String[] args) {
+
         int option;
         Scanner input = new Scanner(System.in);
 
@@ -26,6 +27,34 @@ public class test {
             System.out.println("Goodbye!");
         }
         
+    }
+
+    public static void findMin(){
+
+        int min = array[0];
+
+        for ( int i = 0; i < array.length ; i++ ){
+
+            if( array[i] < min ){
+
+                min = array[i];
+
+            }
+        }
+    }
+
+    public static void findMax(){
+
+        int max;
+        
+        for ( int i = 0; i < array.length ; i++ ){
+
+            if( array[i] > max ){
+
+                max = array[i];
+            }
+        }
+
     }
 
     }
